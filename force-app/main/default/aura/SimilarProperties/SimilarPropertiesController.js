9b821859be9c33ec1bc85a21582f8aa7fe43e9ea
ELTORO.IT <aperez@salesforce.com>
2017-12-23 21:19:47 -0500

    Part 9—Add Design Parameters

--- force-app/main/default/aura/SimilarProperties/SimilarPropertiesController.js
+++ force-app/main/default/aura/SimilarProperties/SimilarPropertiesController.js
@@ -6,8 +6,10 @@
         action.setParams({
             recordId: component.get("v.recordId"),
             beds: component.get("v.property.fields.Beds__c.value"),
-            price: component.get("v.property.fields.Price__c.value")
-        });
+            price: component.get("v.property.fields.Price__c.value"),
+            searchCriteria: component.get("v.searchCriteria"),
+            priceRange: parseInt(component.get("v.priceRange"), 10)
+        });    
         action.setCallback(this, function(response){
             var similarProperties = response.getReturnValue();
             component.set("v.similarProperties", similarProperties);
