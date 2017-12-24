0a954317e1fef03f2347dfbef1d2ea680f2bdf60
ELTORO.IT <aperez@salesforce.com>
2017-12-23 20:45:15 -0500

    Part 5—Edit a Similar Property 'In Place'

--- force-app/main/default/aura/SimilarProperty/SimilarPropertyController.js
+++ force-app/main/default/aura/SimilarProperty/SimilarPropertyController.js
@@ -1,9 +1,16 @@
 ({
-    navToRecord : function (component, event, helper) {
+    navToRecord: function (component, event, helper) {
         var navEvt = $A.get("e.force:navigateToSObject");
         navEvt.setParams({
             "recordId": component.get("v.propertyId")
         });
         navEvt.fire();
+    },
+    editRecord: function (component, event, helper) {
+        var editRecordEvent = $A.get("e.force:editRecord");
+        editRecordEvent.setParams({
+            "recordId": component.get("v.propertyId")
+        });
+        editRecordEvent.fire();
     }
 })
