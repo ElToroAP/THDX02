e3f2347900464f61df9565952c00495f11f927f4
ELTORO.IT <aperez@salesforce.com>
2017-12-23 20:41:15 -0500

    Navigate to a Similar Property

--- force-app/main/default/aura/SimilarProperty/SimilarPropertyController.js
+++ force-app/main/default/aura/SimilarProperty/SimilarPropertyController.js
@@ -1,5 +1,9 @@
 ({
-    myAction : function(component, event, helper) {
-
+    navToRecord : function (component, event, helper) {
+        var navEvt = $A.get("e.force:navigateToSObject");
+        navEvt.setParams({
+            "recordId": component.get("v.propertyId")
+        });
+        navEvt.fire();
     }
 })
