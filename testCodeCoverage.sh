2933def0549ff1377964df6192458c4f1db6bc13
ELTORO.IT <aperez@salesforce.com>
2017-12-24 14:29:44 -0500

    Trying to find code coverage -v0.0.2

--- testCodeCoverage.sh
+++ testCodeCoverage.sh
@@ -4,7 +4,7 @@ echo "Start Code Coverage Calculations"
 varMinCC=75
 #varJQ="jq-osx-amd64"
 varJQ="jq-linux64"
-varCC=`sfdx force:data:soql:query -t -q "SELECT PercentCovered FROM ApexOrgWideCoverage" -r json | ./$varJQ '.result.records[0].PercentCovered'`
+varCC=`sfdx force:data:soql:query -t -q "SELECT PercentCovered FROM ApexOrgWideCoverage" -r json -u CI_SO | ./$varJQ '.result.records[0].PercentCovered'`
 
 #Test results
 if [ "$varCC" -lt "$varMinCC" ] ; then
